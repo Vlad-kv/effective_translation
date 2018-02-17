0a0e152ddb46a06c2d29cb6b02371c0f4fb0d5c8
Киселёв Владислав <vladislav-a-k@mail.ru>
2018-02-17 14:31:47 +0300

    LL1_info
    from  parser_by_hands

---
+++ generator/LL1_info.cpp
@@ -0,0 +1,164 @@
+#include "LL1_info.h"
+#include <cassert>
+#include <functional>
+using namespace std;
+
+bool is_not_terminal(const string& s) {
+	return ((s.size() > 0) && ('A' <= s[0]) && (s[0] <= 'Z'));
+}
+
+LL1_info::rule::rule(const string& not_term, const vector<string>& res)
+: not_term(not_term), res(res) {
+	assert(is_not_terminal(not_term));
+	assert(res.size() > 0);
+	if (res.size() > 1) {
+		for (auto &s : res) {
+			assert(s != "");
+		}
+	}
+}
+
+LL1_info::LL1_info(const string& start_not_term, const vector<rule>& graph, const string& last_sumbol)
+: g(graph), start_not_term(start_not_term), last_sumbol(last_sumbol) {
+	assert(is_not_terminal(start_not_term));
+	
+	not_terminals.insert(start_not_term);
+	
+	for (rule &w : g) {
+		not_terminals.insert(w.not_term);
+		for (string &s : w.res) {
+			if (is_not_terminal(s)) {
+				not_terminals.insert(s);
+			}
+		}
+		rules[w.not_term].insert({w.res, {}});
+	}
+	for (auto &w : not_terminals) {
+		if (rules.count(w) == 0) {
+			throw runtime_error("not_terminal " + w + " has no rules");
+		}
+	}
+	is_update_happened = true;
+	while (is_update_happened) {
+		is_update_happened = false;
+		for (const rule& w : g) {
+			upd_set(first[w.not_term], calc_first(w.res));
+		}
+	}
+	
+	follow[start_not_term].insert(last_sumbol);
+	is_update_happened = true;
+	while (is_update_happened) {
+		is_update_happened = false;
+		for (const rule& w : g) {
+			for (size_t e = 0; e < w.res.size(); e++) {
+				if (is_not_terminal(w.res[e])) {
+					set<string> res = calc_first(w.res, e + 1);
+					if (res.count("") > 0) {
+						res.erase("");
+						upd_set(follow[w.res[e]], follow[w.not_term]);
+					}
+					upd_set(follow[w.res[e]], res);
+				}
+			}
+		}
+	}
+	
+	for (auto &w : rules) {
+		for (const transition &trans : w.second) {
+			trans.terminals_to_identify = calc_first(trans.new_expr);
+			if (trans.terminals_to_identify.count("") > 0) {
+				trans.terminals_to_identify.erase("");
+				upd_set(trans.terminals_to_identify, follow[w.first]);
+			}
+		}
+	}
+}
+void LL1_info::print_map(const set<string>& s, map<string, set<string>>& m) {
+	for (auto &w : s) {
+		cout << w << " : ";
+		for (const string& str : m[w]) {
+			cout << "\"" << str << "\"  ";
+		}
+		cout << "\n";
+	}
+}
+void LL1_info::print_first() {
+	print_map(not_terminals, first);
+}
+void LL1_info::print_follow() {
+	print_map(not_terminals, follow);
+}
+void LL1_info::print_transitions() {
+	for (auto &w : rules) {
+		cout << w.first << ":\n";
+		for (const transition &trans : w.second) {
+			cout << "    ";
+			for (const string& str : trans.new_expr) {
+				cout << "\"" << str << "\" ";
+			}
+			cout << "  :  ";
+			for (const string& str : trans.terminals_to_identify) {
+				cout << "\"" << str << "\" ";
+			}
+			cout << "\n";
+		}
+	}
+}
+LL1_info::transition::transition() {
+}
+LL1_info::transition::transition(const vector<string>& new_expr, const set<string>& terminals_to_identify)
+: new_expr(new_expr), terminals_to_identify(terminals_to_identify){
+}
+bool operator<(const LL1_info::transition& t_1, const LL1_info::transition& t_2) {
+	return (t_1.new_expr < t_2.new_expr);
+}
+void LL1_info::upd_set(set<string>& dest, const string& str) {
+	if (dest.count(str) == 0) {
+		dest.insert(str);
+		is_update_happened = true;
+	}
+}
+void LL1_info::upd_set(set<string>& dest, const set<string>& sourse) {
+	for (auto &w : sourse) {
+		upd_set(dest, w);
+	}
+}
+
+struct on_return {
+	on_return(function<void ()> f)
+	: f(f) {
+	}
+	~on_return() {
+		f();
+	}
+	function<void ()> f;
+};
+
+set<string> LL1_info::calc_first(const vector<string>& v, size_t start_pos) {
+	set<string> res;
+	
+	bool old_val = is_update_happened;
+	on_return on_ret([old_val, this]() {is_update_happened = old_val;});
+	is_update_happened = false;
+	
+	for (size_t w = start_pos; w < v.size(); w++) {
+		const string& str = v[w];
+		if (is_not_terminal(str)) {
+			set<string>& interest = first[str];
+			if (interest.count("") == 0) {
+				upd_set(res, interest);
+				return res;
+			} else {
+				interest.erase("");
+				upd_set(res, interest);
+				interest.insert("");
+			}
+		} else {
+			upd_set(res, str);
+			return res;
+		}
+	}
+	upd_set(res, "");
+	return res;
+}
---
+++ generator/LL1_info.h
@@ -0,0 +1,55 @@
+#ifndef LL1_INFO_H
+#define LL1_INFO_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+// Нетерминал начинается с большой латинской буквы, "эпсилон" - пустая строка.
+
+bool is_not_terminal(const std::string& s);
+
+struct LL1_info {
+    struct rule {
+        rule(const std::string& not_term, const std::vector<std::string>& res);
+        
+        std::string not_term;
+        std::vector<std::string> res;
+    };
+    
+	LL1_info(const std::string& start_not_term, const std::vector<rule>& graph, const std::string& last_sumbol = "$");
+	
+	static void print_map(const std::set<std::string>& s, std::map<std::string, std::set<std::string>>& m);
+	void print_first();
+	void print_follow();
+	void print_transitions();
+	
+	std::vector<rule> g;
+	std::string start_not_term, last_sumbol;
+	
+	struct transition {
+		transition();
+		transition(const std::vector<std::string>& new_expr, const std::set<std::string>& terminals_to_identify);
+		
+		friend bool operator<(const transition& t_1, const transition& t_2);
+		
+		std::vector<std::string> new_expr;
+		mutable std::set<std::string> terminals_to_identify;
+	};
+	
+	std::set<std::string> not_terminals;
+	std::map<std::string, std::set<transition>> rules;
+	
+	std::map<std::string, std::set<std::string>> first;
+	std::map<std::string, std::set<std::string>> follow;
+private:
+	bool is_update_happened;
+	
+	void upd_set(std::set<std::string>& dest, const std::string& str);
+	void upd_set(std::set<std::string>& dest, const std::set<std::string>& sourse);
+	
+	std::set<std::string> calc_first(const std::vector<std::string>& v, size_t start_pos = 0);
+};
+
+#endif // LL1_INFO_H
